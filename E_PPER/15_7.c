4ad6eb54c58b44603585e7a3a8fc289c0c9786e5
Jiwon Yoon <10348029+JiWon0502@users.noreply.github.com>
2021-03-17 15:04:49 +0900

    Add files via upload

--- E_PPER/15_7.c
+++ E_PPER/15_7.c
@@ -1,128 +1,49 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
-
-int s[1000];
-int e[1000];
 //프로그래머스에서는 main함수 및 입출력문이 필요하지 않습니다. 대신 solution함수만 작성하면 됩니다.
-int compare_s(const void* a, const void* b){
-	if(s[*(int*)a] > s[*(int*)b]) return 1;
-	else if(s[*(int*)a] < s[*(int*)b]) return -1;
-	else return 0;
-}
-int compare_e(const void* a, const void* b){
-	if(e[*(int*)a] > e[*(int*)b]) return 1;
-	else if(e[*(int*)a] < e[*(int*)b]) return -1;
-	else return 0;
-}
-
 int solution(int s[], int e[], int N){
-	int answer = 0, i = N;
-	int l_fnl = 0, r_fnl = 0;	
-	int* indx = malloc(sizeof(int)*N);
-	while(i-->=0) indx[i] = i;
-	printf("%d %d %d %d %d\n", indx[0], indx[1], indx[2], indx[3], indx[4]);
-	
-	if(N==1 || N==2) return N;
-	
-	for(int i = 1; i <= N; i++) *indx = i;
-	qsort(indx, N, sizeof(int), compare_s);
+    int answer = 0;
+    int n[1000];
+    int e_n[1000];
+    int i, j, tmp = 0, e1 = -1, e2 = -1;
+    for(i = 0; i < N; i++){
+        for(j = 0; j < N; j++){
+            if(e[j] < e[tmp]) tmp = j;
+						else if(e[j] == e[tmp] && s[j]<s[tmp]) tmp = j;
+        }
+        e_n[tmp] = e[tmp];
+        e[tmp] = 86400;
+        n[i] = tmp;
+    }
 
-
-	qsort(indx, N, sizeof(int), compare_e);
-
-
 	
-	l_fnl = e[indx[i++]];
-	r_fnl = e[indx[i++]];
-	answer += 2;
-	
-	while(i<N){
-		if(s[indx[i]] >= l_fnl){
-			answer++;
-			l_fnl = s[indx[i++]];
-		}
-		else if(s[indx[i]] >= r_fnl){
-			answer++;
-			l_fnl = s[indx[i++]];
-		}
-		else i++;
-	}
-	
-	return answer;
+    for(i = 0; i < N; i++){
+        if(s[n[i]] >= e1){
+          answer++;
+          e1 = e_n[n[i]];
+        }
+        else if(s[n[i]] >= e2){
+          answer++;
+					e2 = e1;
+          e1 = e_n[n[i]];
+        }
+    }
+    return answer;
 }
+
 
 int main() {
-
-	int N = 0;
-	scanf("%d", &N);
-	for(int i = 0; i < N; i++)
-		scanf("%d", &s[i]);
-	for(int i = 0; i < N; i++)
-		scanf("%d", &e[i]);
+    int s[1000];
+    int e[1000];
+    int N = 0;
+    scanf("%d", &N);
+    for(int i = 0; i < N; i++)
+        scanf("%d", &s[i]);
+    for(int i = 0; i < N; i++)
+        scanf("%d", &e[i]);
 	
-	printf("%d", solution(s, e, N));
-	return 0;
-}#include <stdio.h>
-#include <stdbool.h>
-#include <stdlib.h>
-
-int s[1000];
-int e[1000];
-//프로그래머스에서는 main함수 및 입출력문이 필요하지 않습니다. 대신 solution함수만 작성하면 됩니다.
-int compare_s(const void* a, const void* b){
-	if(s[*(int*)a] > s[*(int*)b]) return 1;
-	else if(s[*(int*)a] < s[*(int*)b]) return -1;
-	else return 0;
-}
-int compare_e(const void* a, const void* b){
-	if(e[*(int*)a] > e[*(int*)b]) return 1;
-	else if(e[*(int*)a] < e[*(int*)b]) return -1;
-	else return 0;
+    printf("%d", solution(s, e, N));
+    return 0;
 }
 
-int solution(int s[], int e[], int N){
-	int answer = 0, i = N;
-	int l_fnl = 0, r_fnl = 0;	
-	int* indx = malloc(sizeof(int)*N);
-	while(i-->=0) indx[i] = i;
-	printf("%d %d %d %d %d\n", indx[0], indx[1], indx[2], indx[3], indx[4]);
-	
-	if(N==1 || N==2) return N;
-	
-	for(int i = 0; i < N; i++) *indx = i;
-	qsort(indx, N, sizeof(int), compare_s);
-	qsort(indx, N, sizeof(int), compare_e);
-	
-	
-	l_fnl = e[indx[i++]];
-	r_fnl = e[indx[i++]];
-	answer += 2;
-노	
-	while(i<N){
-		if(s[indx[i]] >= l_fnl){
-			answer++;
-			l_fnl = s[indx[i++]];
-		}
-		else if(s[indx[i]] >= r_fnl){
-			answer++;
-			l_fnl = s[indx[i++]];
-		}
-		else i++;
-	}
-	
-	return answer;
-}
-
-int main() {
-
-	int N = 0;
-	scanf("%d", &N);
-	for(int i = 0; i < N; i++)
-		scanf("%d", &s[i]);
-	for(int i = 0; i < N; i++)
-		scanf("%d", &e[i]);
-	
-	printf("%d", solution(s, e, N));
-	return 0;
-}
